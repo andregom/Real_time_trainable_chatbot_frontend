fb143b2d7c3ee12accbfba41bf11bb4b4947772f
Andrezj <andre.oliveira@ccc.ufcg.edu.br>
2023-05-06 17:21:53 -0300

    usex hash routing style

--- src/routing/ApplicationRouter.tsx
+++ src/routing/ApplicationRouter.tsx
@@ -14,19 +14,19 @@ const ApplicationRouter: React.FC = () => {
             <NavBar />
             <Routes>
                 {/* Public */}
-                {/* {<Route path="/user-login" element={<UserLogin />} />} */}
+                {/* {<Route path="user-login" element={<UserLogin />} />} */}
 
                 {/* Private */}
                 <Route element={<RequireAuth />}>
-                    <Route path="/" element={
-                        <Navigate to="/admin" />
+                    <Route path="" element={
+                        <Navigate to="admin" />
                     } />
-                    <Route path='/admin' element={<AdminsPage />} />
-                    <Route path='/user' element={<UsersPage />} />
-                    <Route path='/talk' element={<ChatPage />} />
-                    <Route path='/register' element={<IssuesPage />} />
-                    <Route path='/edit-database' element={<DatabasePage editPermission={true}/>} />
-                    <Route path='/view-database' element={<DatabasePage editPermission={false}/>} />
+                    <Route path='admin' element={<AdminsPage />} />
+                    <Route path='user' element={<UsersPage />} />
+                    <Route path='talk' element={<ChatPage />} />
+                    <Route path='register' element={<IssuesPage />} />
+                    <Route path='edit-database' element={<DatabasePage editPermission={true}/>} />
+                    <Route path='view-database' element={<DatabasePage editPermission={false}/>} />
                 </Route>
             </Routes>
         </>
