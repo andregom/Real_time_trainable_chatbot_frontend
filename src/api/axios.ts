7dbbfebbcfc493c5695346fee3f224eca8907ce4
Andrezj <andre.oliveira@ccc.ufcg.edu.br>
2024-05-04 18:45:25 -0300

    adapts endpionts to env and prod mode at runtime

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://13.59.254.151:5000';
+const PROD_URL = 'https://13.59.254.151';
+
+const DEV_URL =  'http://localhost:5000';
+
+const IS_DEV_ENV = !process.env.NODE_ENV 
+    || process.env.NODE_ENV === 'development';
+
+export const BASE_URL = IS_DEV_ENV ? DEV_URL : PROD_URL;
 
 export default axios.create({
     baseURL: BASE_URL
--- src/service/queryClient.ts
+++ src/service/queryClient.ts
@@ -1,7 +1,14 @@
 import { axiosPrivate } from "../api/axios";
 import { QueryClient } from "react-query";
 
-export const BASE_URL = 'http://13.59.254.151:5000';
+const PROD_URL = 'https://13.59.254.151';
+
+const DEV_URL =  'http://localhost:5000';
+
+const IS_DEV_ENV = !process.env.NODE_ENV 
+    || process.env.NODE_ENV === 'development';
+
+export const BASE_URL = IS_DEV_ENV ? DEV_URL : PROD_URL;
 
 export const queryClient = new QueryClient({
     defaultOptions: {
